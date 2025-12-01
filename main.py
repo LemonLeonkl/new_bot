f129921123b1d2e39beb406b03bdf45da0aa3b8f
LemonLeonkl <leonid.utyashev@gmail.com>
2025-12-01 18:44:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-from config import token 
-import random
-import telebot
+import telebot # библиотека telebot
 
-bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет, я новая верся бота от Лёнчика")
+bot = telebot.TeleBot("token") 
 
-@bot.message_handler(commands=['bye'])
-def send_bye(message):
-    bot.reply_to(message, "Пока!")
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
-@bot.message_handler(commands=['random_number'])
-def random_number(message):
+@bot.message_handler(commands=['ban'])
+def ban_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+         # проверка пользователя
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+bot.infinity_polling(none_stop=True)
+
     n = int(message.text.split()[1]) if len(message.text.split()) > 1 else 10
     number = random.randint(1, n)
     bot.reply_to(message, number)
